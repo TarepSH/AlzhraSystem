6845692f8252468d3a86c1c67b866e872b9cbae3
TarepSH <tarepsh@gmail.com>
2019-06-02 04:55:03 +0300

    chaing the model name from students to student

--- app/models/student.rb
+++
@@ -1,8 +0,0 @@
-class Student < ApplicationRecord
-  belongs_to :classroom
-  belongs_to :activities
-  belongs_to :teacher
-  belongs_to :attendings
-  belongs_to :memorizationparts
-  belongs_to :memorizationpage
-end
