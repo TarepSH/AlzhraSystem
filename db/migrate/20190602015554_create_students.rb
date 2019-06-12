197e4cfb5a2b212a3f432075548879ca933a67f2
TarepSH <tarepsh@gmail.com>
2019-06-12 03:38:26 +0300

    chaining in clarrroom table for some error in herok migration 2

--- db/migrate/20190602015554_create_students.rb
+++ db/migrate/20190602015554_create_students.rb
@@ -12,12 +12,6 @@ class CreateStudents < ActiveRecord::Migration[5.2]
       t.integer :mobilephone
       t.integer :linephone
       t.integer :point
-      t.references :classroom, foreign_key: true
-      t.references :activities, foreign_key: true
-      t.references :teacher, foreign_key: true
-      t.references :attendings, foreign_key: true
-      t.references :memorizationparts, foreign_key: true
-      t.references :memorizationpage, foreign_key: true
 
       t.timestamps
     end
