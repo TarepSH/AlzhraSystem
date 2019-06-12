5d1db9758fbb54587c33371d88abd94fc8f3f366
TarepSH <tarepsh@gmail.com>
2019-06-12 03:04:02 +0300

    reset my databas

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_11_231624) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "attendings", force: :cascade do |t|
     t.date "day"
     t.datetime "created_at", null: false
