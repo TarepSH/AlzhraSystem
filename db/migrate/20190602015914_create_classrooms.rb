ca24093dcf9cfb0cd9f21e3fe691f4b2dd7bb922
TarepSH <tarepsh@gmail.com>
2019-06-12 03:45:46 +0300

    chaining in clarrroom table for some error in herok migration 4

--- db/migrate/20190602015914_create_classrooms.rb
+++ db/migrate/20190602015914_create_classrooms.rb
@@ -4,7 +4,6 @@ class CreateClassrooms < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :book
       t.integer :level
-      t.references :teacher, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20190602023745_remove_teacherid_from_student.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTeacheridFromStudent < ActiveRecord::Migration[5.2]
-  def change
-    remove_reference :students, :teacher, foreign_key: true
-  end
-end
